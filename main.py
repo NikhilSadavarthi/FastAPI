b5030d7af830df0a3f64028a0ad8bdcd5edaa9f7
NikhilSadavarthi <nikhilsadavarti@gmail.com>
2026-06-26 09:22:28 +0530

    search patient from id feature added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import json
 app = FastAPI()
 
@@ -16,3 +16,14 @@ def view():
     data = load_data()
 
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of patient', example='P001')):
+
+    #load data first
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        raise HTTPException('status_code=404', detail='patient not found')

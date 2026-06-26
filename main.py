4ca1c408dc6401076075fe20858156ceab149d49
NikhilSadavarthi <nikhilsadavarti@gmail.com>
2026-06-26 17:39:25 +0530

    sorting feature added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException,Query
 import json
 app = FastAPI()
 
@@ -27,3 +27,22 @@ def view_patient(patient_id: str = Path(..., description='ID of patient', exampl
         return data[patient_id]
     else:
         raise HTTPException(status_code=404, detail='patient not found')
+
+@app.get('/sort')
+def sort_patient(sort_by:str = Query(..., description='sort on the basis of height,weight or bmi'), order : str = Query('asc', description='sort in asc or desc')):
+
+    valid_fields= ['height','weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from{valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400 , detail='Invalid order select between asc or desc')
+    
+    data = load_data()
+
+    sort_order = True if order == 'desc' else False
+
+    sorted_data = sorted(data.values(), key= lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
